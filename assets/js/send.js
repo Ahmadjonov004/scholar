b942b124d1b51669353464c8369815ee376c441b
Ahmadjonov004 <ahmadjonovasilbek624@gmail.com>
2025-01-10 21:34:59 +0500

    send.js ni qoshdim

---
+++ assets/js/send.js
@@ -0,0 +1,31 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault(); 
+  
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+  
+    const botToken = '7427307380:AAFwkxZqOL5QQTZBrATPFA27GZXMr17TJMM'; 
+    const chatId = '5774362777'; 
+  
+    const text = `👤 Имя: ${name}\n📧 Email: ${email}\n💬 Сообщение: ${message}`;
+  
+    fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ chat_id: chatId, text: text })
+    })
+      .then(response => {
+        if (response.ok) {
+          alert('Сообщение успешно отправлено в Telegram!');
+          document.getElementById('contact-form').reset(); 
+        } else {
+          alert('Произошла ошибка при отправке сообщения.');
+        }
+      })
+      .catch(error => {
+        console.error('Ошибка:', error);
+        alert('Не удалось отправить сообщение.');
+      });
+  });
+  
